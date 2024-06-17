1a5ea25a59a2f927efea9c528b164c37d67cd571
Jack Webdev <jackbuda31@gmail.com>
2024-06-17 05:59:46 +0200

    custom favicon, bg-gradient

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,13 +2,15 @@ import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
 
-const poppins = Poppins({ subsets: ["latin"],style: "italic", weight: "400" });
-
+const poppins = Poppins({ subsets: ["latin"], style: "italic", weight: "400" });
 
 export const metadata: Metadata = {
   title: "Jack WebDev",
   description:
     "Jack WebDev is a software developer who builds accessible, inclusive products and digital experiences for the web.",
+    icons: {
+      icon: "/Favicon.png"
+    }
 };
 
 export default function RootLayout({
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Navbar() {
   return (
     <nav className={` flex justify-around items-center mt-12 lg:ml-[10rem]`}>
-        <Link href="/" className={` text-[#6666FF] text-5xl font-bold hover:text-[#FFF]`}>Jack WebDev</Link>
+        <Link href="/" className={` text-[#6666FF] text-5xl font-bold hover:text-[#FFF]`}>JW</Link>
 
         <a className="text-white text-xl font-bold hover:text-[#BB33FF]" href="/cv.pdf" download={"cv.pdf"}>RESUME</a>
     </nav>
