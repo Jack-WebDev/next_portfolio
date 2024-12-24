a4516c9dace3bbb19dde776636bf044a2504795c
Jack-WebDev <jackbuda31@gmail.com>
2024-12-24 20:31:38 +0200

    :sparkles: add project

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ProjectCards from "./ProjectCards";
 import eCommercePage from "@/assets/ecommercePage.png";
+import audiophile from "@/assets/audiophile.png";
 
 export default function Projects() {
   return (
@@ -14,6 +15,13 @@ export default function Projects() {
         description='This e-commerce product page highlights a "Fall Limited Edition Sneakers" offering. It features a clean layout with a prominent product image on the left and detailed product information on the right. There are interactive quantity buttons, a clear call-to-action "Add to Cart" button in orange, and a navigation bar at the top for seamless browsing.'
         link="https://e-commerce-product-page-neon-theta.vercel.app/"
         tools={["React-Typescript", "Vite", "TailwindCSS", "Framer Motion"]}
+      />
+      <ProjectCards
+        img={audiophile}
+        title="Audiophile E-Commerce"
+        description="This project is an e-commerce platform specializing in the sale of speakers, earphones, and headphones, designed to provide a seamless shopping experience. Users can easily add or remove items from their cart, adjust product quantities, and proceed through a checkout process with real-time form validation for accuracy."
+        link="https://audiophile-eta-ten.vercel.app/"
+        tools={["Next.js 14", "TailwindCSS", "Shadcn UI", "Framer Motion"]}
       />
     </div>
   );
